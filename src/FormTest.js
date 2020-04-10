a6c0abbbec9180e7935f6affa00a1b06109dc383
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-10 08:53:27 -0400

    Testing ContacForm working

--- src/FormTest.js
+++ src/FormTest.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("Type input", () => {
   //ARRANGE: Set up any test data that we want in the DOM
-
+  const { getByText } = render(<ContactForm />);
   //Act action that the USER does that we want to test
   //fill out the form
   //click submit button
 
   //ASSERT: test that the data or app responded the way you expected
   //testing that is visible for the user
-
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/add new name/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerText = getByText(/current type name/i);
+  expect(headerText).toBeVisible();
+  //   const { getByText } = render(<App />);
+  //   const linkElement = getByText(/add new name/i);
+  //   expect(linkElement).toBeInTheDocument();
 });
